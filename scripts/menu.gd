506ed5c25298d1256d1525d0425f187f15b8982e
jptarco <128825517+jptarco@users.noreply.github.com>
2026-02-17 15:14:00 -0500

    Rename demo to main

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,5 +1,5 @@
 extends Control
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/demo.tscn")
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 	pass # Replace with function body.

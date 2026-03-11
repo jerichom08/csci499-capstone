10d6e4f124ebedf80d5c168600e73381519449d9
Alexander-col <aexro2221@gmail.com>
2026-03-11 10:48:00 -0400

    puzzle 2 and interaction working

--- scenes/puzzles/puzzle_2/ingredient_station.gd
+++ scenes/puzzles/puzzle_2/ingredient_station.gd
@@ -9,14 +9,14 @@ signal ingredient_selected(ingredient_name)
 var player_in_range : bool = false
 
 @onready var interaction_label = $InteractionLabel
-@onready var audio_player = $AudioStreamPlayer2D
+@onready var audio_player = $AudioStreamPlayer2D2
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 	
 	if interaction_label:
-		interaction_label.visable = false
+		interaction_label.visible = false
 		
 func _process(delta: float) -> void:
 	if player_in_range and Input.is_action_just_pressed(interact_action):
@@ -34,12 +34,12 @@ func _on_body_entered(body: Node) -> void:
 	if body.name == "Player":
 		player_in_range = true;
 		
-	if interaction_label:
-		interaction_label.visable = true
+		if interaction_label:
+			interaction_label.visible = true
 		
 func _on_body_exited(body: Node) -> void:
 	if body.name == "Player":
 		player_in_range = false
 		
 		if interaction_label:
-			interaction_label.visable = false
+			interaction_label.visible = false

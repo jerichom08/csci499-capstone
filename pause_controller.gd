c30346611c0b2a20fb9b67837f57af45a6e76dd4
jatinkapoor1021 <31459506+jatinkapoor1021@users.noreply.github.com>
2026-03-15 14:10:12 -0400

    Create pause_controller.gd

---
+++ pause_controller.gd
@@ -0,0 +1,11 @@
+extends Node
+
+@onready var pause_menu = $PauseMenu
+
+func _input(event):
+
+    if event.is_action_pressed("ui_cancel"):
+        if get_tree().paused:
+            pause_menu.resume()
+        else:
+            pause_menu.pause()

b0c9959e248776d82164e739df97519f4e85672c
jatinkapoor1021 <31459506+jatinkapoor1021@users.noreply.github.com>
2026-03-23 14:03:17 -0400

    Update pause_controller.gd

--- pause_controller.gd
+++ pause_controller.gd
@@ -5,6 +5,10 @@ extends Node
 func _input(event):
 
     if event.is_action_pressed("ui_cancel"):
+
+        if get_tree().current_scene.name == "MainMenu":
+            return
+
         if pause_menu.visible:
             pause_menu.resume()
         else:

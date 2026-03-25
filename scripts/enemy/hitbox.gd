b0b33cfec9ce2dc681f38fc3df81b39774ea07b8
Jericho <jerichomaniago@gmail.com>
2026-03-25 00:03:56 -0400

    Revert "Merge branch 'main' into player"
    This reverts commit 32930ebeddaa9530d4efd51354080120285b75f0, reversing
    changes made to 9833ed551334951978ae6ac6e044c37e7380985c.

--- scripts/enemy/hitbox.gd
+++ scripts/enemy/hitbox.gd
@@ -6,13 +6,5 @@ func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area: Area2D) -> void:
-	print("detected an Area: ", area)
-	print("name: ", area.name)
-	print("scene path: ", area.get_path())
-	print("script: ", area.get_script())
-
 	if area is Hurtbox:
-		print("detected a Hurtbox")
 		area.take_hit(damage)
-	else:
-		print("this area is NOT a Hurtbox")

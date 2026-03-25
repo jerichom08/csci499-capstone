d29d9bf22ed3d814a807076b84e606632d10b945
Alexander-col <aexro2221@gmail.com>
2026-03-24 23:36:32 -0400

    changed script for clue to be interact pop up

--- scenes/Components/clue_sign.gd
+++ scenes/Components/clue_sign.gd
@@ -1,12 +1,8 @@
 extends Area2D
 
-@export var interact_action : String = "interact"
-
 var player_in_range : bool = false
-var clue_open : bool = false
 
 @onready var interaction_label = $InteractionLabel
-@onready var clue_text = $ClueText
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -15,37 +11,20 @@ func _ready() -> void:
 	if interaction_label:
 		interaction_label.visible = false
 
-	if clue_text:
-		clue_text.visible = false
-
-func _process(_delta: float) -> void:
-	if player_in_range and Input.is_action_just_pressed(interact_action):
-		_toggle_clue()
-
-func _toggle_clue() -> void:
-	clue_open = not clue_open
-
-	if clue_text:
-		clue_text.visible = clue_open
-
-	if interaction_label:
-		interaction_label.visible = not clue_open and player_in_range
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
 		player_in_range = true
 
-		if interaction_label and not clue_open:
+		# Show the text when player is near
+		if interaction_label:
 			interaction_label.visible = true
+
 
 func _on_body_exited(body: Node) -> void:
 	if body.is_in_group("player"):
 		player_in_range = false
 
+		# Hide the text when player leaves
 		if interaction_label:
 			interaction_label.visible = false
-
-		if clue_text:
-			clue_text.visible = false
-
-		clue_open = false

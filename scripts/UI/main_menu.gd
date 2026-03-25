b5363d8ca763cbbcf5fb1bf8796f8a46dddb771a
Jericho <jerichomaniago@gmail.com>
2026-03-25 13:39:36 -0400

    canvas signals connected

--- scripts/UI/main_menu.gd
+++ scripts/UI/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_button_pressed():
-    get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
+	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
 
 func _on_quit_button_pressed():
-    get_tree().quit()
+	get_tree().quit()

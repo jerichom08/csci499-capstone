c524ef76038a79ba3127a26a814ff2f59ca9ea49
Naima <naemxa22@gmail.com>
2026-04-15 02:47:37 -0400

    new rooms added

---
+++ scenes/levels/level_1/next_scene_1.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+const FILE_BEGIN = "res://scenes/levels/level_1/scene_"
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		var current_scene_file = get_tree().current_scene.scene_file_path
+		
+		var file_name = current_scene_file.get_file()
+		
+		var number_str = file_name.replace("scene_", "").replace(".tscn", "")
+		
+		var next_level_number = int(number_str) + 1
+
+		var next_level_path = FILE_BEGIN + str(next_level_number) + ".tscn"
+		get_tree().change_scene_to_file(next_level_path)

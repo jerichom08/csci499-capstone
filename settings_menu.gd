05b00bef87616b633b1117ec76da31cc802b7edb
jatinkapoor1021 <31459506+jatinkapoor1021@users.noreply.github.com>
2026-04-14 23:23:07 -0400

    Updating settings_menu.gd for it to work with the scene

--- settings_menu.gd
+++ settings_menu.gd
@@ -1,7 +1,19 @@
 extends Control
 
+@onready var volume_slider = $VBoxContainer/VolumeSlider
+@onready var back_button = $VBoxContainer/BackButton
+
 func _ready():
+    # Set default volume (optional)
+    volume_slider.value = db_to_linear(AudioServer.get_bus_volume_db(0))
+
+    # Make sure UI works while paused
     pause_mode = Node.PAUSE_MODE_PROCESS
 
+# 🎧 Volume control
+func _on_volume_slider_value_changed(value):
+    AudioServer.set_bus_volume_db(0, linear_to_db(value))
+
+# 🔙 Back button
 func _on_back_button_pressed():
-    get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
+    get_tree().change_scene_to_file("res://scenes/menu.tscn")

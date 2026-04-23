19d72079aaa5c48eaf752030acdd727803d72761
jptarco <128825517+jptarco@users.noreply.github.com>
2026-04-23 14:11:46 -0400

    Adding Attack Logic

--- scripts/enemy/frog.gd
+++ scripts/enemy/frog.gd
@@ -1,23 +1,77 @@
-extends CharacterBody2D
+extends EnemyBase
 
-@export var frog_spit_attack: PackedScene
-
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-
-var facing_direction = 1
+var was_defeated : bool = false
+var can_attack : bool = false
+var dx : int
 
 func _ready() -> void:
-	sprite.play("light_attack")
+	super._ready()
+	set_state(State.REST)
+
+func _physics_process(_delta: float) -> void:
+	print(state_animations[current_state])
+	update_state_machine()
+	move_and_slide()
+
+func handle_idle() -> void:
+	set_state(State.IDLE)
+	if can_see_player():
+		handle_chase()
+
+func handle_rest() -> void:
+	set_state(State.REST)
+	if can_see_player():
+		handle_idle()
+
+func handle_chase() -> void:
+	set_state(State.CHASE)
 	
-	var spit = $FrogSpit
-	var spit_sprite = spit.get_node("AnimatedSprite2D")
+	var player : Node2D = get_player()
+	if player == null:
+		velocity.x = 0
+		return
 	
-	spit.global_position = $SpitSpawn.global_position
-	spit.visible = true
+	var dir : int = sign(player.global_position.x - global_position.x)
+	face_direction(dir)
 	
-	if facing_direction < 0:
-		spit.scale.x = -abs(spit.scale.x)
+	velocity.x = dir * stats.move_speed
+
+func handle_attack() -> void:
+	#var player : Node2D = get_player()
+	#if player == null:
+		#dx = abs(player.global_position.x - global_position.x)
+	#
+	#if dx <= 200:
+		#set_attack(AttackType.LIGHT)
+	#else:
+		#set_attack(AttackType.HEAVY)
+	#
+	#handle_chase()
+	pass
+
+func handle_hurt(damage : int) -> void:
+	health = max(0, health - damage)
+	if health == 0:
+		handle_defeat()
+		return
+	
+	set_state(State.HURT)
+	handle_chase()
+
+func handle_defeat() -> void:
+	if was_defeated:
+		set_state(State.DEFEAT)
 	else:
-		spit.scale.x = abs(spit.scale.x)
+		was_defeated = true
+		handle_heal(stats.max_health)
+
+func handle_heal(hp : int) -> void:
+	if was_defeated:
+		# Spawn spiders to revive completely
+		return
 	
-	spit_sprite.play("spit")
+	health = min(stats.max_health, health + hp)
+	set_state(State.HEAL)
+
+func take_damage(damage : int) -> void:
+	handle_hurt(damage)

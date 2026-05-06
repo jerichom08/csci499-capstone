6af4a316dea3d543081dccb363467c49f0981909
Naima <naemxa22@gmail.com>
2026-05-06 19:08:09 -0400

    lv2 s3

---
+++ scenes/lvInterface/hidden_platform.gd
@@ -0,0 +1,54 @@
+extends StaticBody2D
+
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var sound_player: AudioStreamPlayer2D = $sound_player
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+var is_running := false
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") and not is_running:
+		disappear()
+		
+
+func disappear() -> void:
+	is_running = true
+	
+	var tween = create_tween()
+	# wait
+	tween.tween_interval(1.5)
+	
+	tween.tween_callback(func(): sound_player.play())
+	
+	# Fade out
+	
+	tween.tween_property(self, "modulate:a", 0.0, 0.4)
+	
+	tween.tween_callback(func(): collision_shape_2d.disabled = true)
+	
+	# wait 
+	tween.tween_interval(3.0)
+	
+	# Re-enable collision
+	tween.tween_callback(func(): collision_shape_2d.disabled = false)
+	
+	# Fade back in
+	tween.tween_property(self, "modulate:a", 1.0, 0.4)
+	
+	# Reset flag
+	tween.tween_callback(func(): is_running = false)
+
+
+func _on_ready() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 0.0, 0)
+	tween.tween_callback(func(): collision_shape_2d.disabled = true)
+	
+	
+
+
+func _on_button_button_pressed() -> void:
+	var tween = create_tween()
+	tween.tween_callback(func(): collision_shape_2d.disabled = false)
+	tween.tween_property(self, "modulate:a", 1.0, 0.4)
+	

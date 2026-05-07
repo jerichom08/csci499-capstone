dbc75925a1d96c99727190a088bc3725e364a3f3
Naima <naemxa22@gmail.com>
2026-05-06 22:48:41 -0400

    hidden platform implementation

--- scenes/lvInterface/hidden_platform.gd
+++ scenes/lvInterface/hidden_platform.gd
@@ -1,54 +1,74 @@
 extends StaticBody2D
 
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var platform_collision: CollisionShape2D = $CollisionShape2D
+@onready var area_collision: CollisionShape2D = $Area2D/CollisionShape2D
 @onready var sound_player: AudioStreamPlayer2D = $sound_player
-@onready var sprite_2d: Sprite2D = $Sprite2D
 
 var is_running := false
+var is_active := false
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player") and not is_running:
-		disappear()
-		
+func _ready() -> void:
+	hide_platform()
+
+
+func hide_platform() -> void:
+	modulate.a = 0.0
+	platform_collision.set_deferred("disabled", true)
+	area_collision.set_deferred("disabled", true)
+	is_active = false
+
+
+func show_platform() -> void:
+	if is_active or is_running:
+		return
+	
+	is_active = true
+	
+	sound_player.play()
+	
+	platform_collision.set_deferred("disabled", false)
+	area_collision.set_deferred("disabled", false)
+	
+	var tween := create_tween()
+	tween.tween_property(self, "modulate:a", 1.0, 0.4)
+
 
 func disappear() -> void:
+	if is_running:
+		return
+	
 	is_running = true
 	
-	var tween = create_tween()
-	# wait
+	var tween := create_tween()
+	
+	# Wait after player steps on it
 	tween.tween_interval(1.5)
 	
-	tween.tween_callback(func(): sound_player.play())
+	# Play sound again when disappearing
+	tween.tween_callback(func():
+		sound_player.play()
+	)
 	
 	# Fade out
-	
 	tween.tween_property(self, "modulate:a", 0.0, 0.4)
 	
-	tween.tween_callback(func(): collision_shape_2d.disabled = true)
-	
-	# wait 
-	tween.tween_interval(3.0)
-	
-	# Re-enable collision
-	tween.tween_callback(func(): collision_shape_2d.disabled = false)
-	
-	# Fade back in
-	tween.tween_property(self, "modulate:a", 1.0, 0.4)
-	
-	# Reset flag
-	tween.tween_callback(func(): is_running = false)
+	# Turn collision off
+	tween.tween_callback(func():
+		platform_collision.set_deferred("disabled", true)
+		area_collision.set_deferred("disabled", true)
+		is_active = false
+		is_running = false
+	)
 
 
-func _on_ready() -> void:
-	var tween = create_tween()
-	tween.tween_property(self, "modulate:a", 0.0, 0)
-	tween.tween_callback(func(): collision_shape_2d.disabled = true)
-	
-	
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player") and is_active and not is_running:
+		disappear()
 
 
 func _on_button_button_pressed() -> void:
-	var tween = create_tween()
-	tween.tween_callback(func(): collision_shape_2d.disabled = false)
-	tween.tween_property(self, "modulate:a", 1.0, 0.4)
-	
+	show_platform()
+
+
+func _on_button_2_button_pressed() -> void:
+	show_platform()

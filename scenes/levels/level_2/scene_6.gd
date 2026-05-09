8f3903b263968fb5306ccb2d48edd221352aabdd
Naima <naemxa22@gmail.com>
2026-05-08 21:19:10 -0400

    lv 2 complete ?

---
+++ scenes/levels/level_2/scene_6.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	SceneTransition.change_scene_to("res://scenes/levels/level_3/scene_1.tscn")
+	

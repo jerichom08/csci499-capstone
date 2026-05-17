62e8659ddb1f8c841abf8e891552c6c9f0a6b7bd
Naima <naemxa22@gmail.com>
2026-05-16 20:05:15 -0400

    lv3 complete

---
+++ scenes/levels/level_3/scene_6.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var p: TileMapLayer = $"path up"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	p.visible = false
+	p.collision_enabled = false
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	SceneTransition.change_scene_to("res://scenes/levels/level_4/scene_1.tscn")
+	
+#joey needs to connect this signal
+func _on_boss_defeated() -> void:
+	p.visible = true
+	p.collision_enabled = true

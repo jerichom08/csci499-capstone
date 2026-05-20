0934eb36be3a860bc09af9baec0b85504314e720
Jericho <jerichomaniago@gmail.com>
2026-05-20 12:57:45 -0400

    added pause menu

--- scripts/UI/pause_menu.gd
+++ scripts/UI/pause_menu.gd
@@ -1,31 +1,44 @@
-extends Control
+extends CanvasLayer
+
+@onready var restart_button = $PausePanel/MarginContainer/VBoxContainer/Restart
+@onready var main_menu_button = $PausePanel/MarginContainer/VBoxContainer/MainMenu
 
 func _ready():
 	visible = false
-	pause_mode = Node.PAUSE_MODE_PROCESS
-
-func pause():
-	get_tree().paused = true
-	visible = true
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	
-	modulate.a = 0
-	create_tween().tween_property(self, "modulate:a", 1, 0.2)
+	restart_button.process_mode = Node.PROCESS_MODE_ALWAYS
+	main_menu_button.process_mode = Node.PROCESS_MODE_ALWAYS
 
-func resume():
+	restart_button.pressed.connect(_on_restart_level_pressed)
+	main_menu_button.pressed.connect(_on_main_menu_pressed)
+
+func _input(event):
+	var current_scene = get_tree().current_scene
+	
+	if current_scene == null:
+		return
+	
+	if current_scene.scene_file_path == "res://scenes/main_menu.tscn" or current_scene.scene_file_path == "res://scenes/LevelSelect.tscn" or current_scene.scene_file_path == "res://scenes/Fin.tscn" or current_scene.scene_file_path == "res://scenes/levels/tutorial/room.tscn":
+		return
+	
+	if event.is_action_pressed("pause"):
+		toggle_pause()
+
+func toggle_pause():
+	get_tree().paused = !get_tree().paused
+	visible = get_tree().paused
+
+func _on_restart_level_pressed():
+	print("Restart button clicked")
 	get_tree().paused = false
 	visible = false
+	var player = get_tree().get_first_node_in_group("player")
+	
+	if player and player.has_method("reset_room"):
+		player.reset_room()
 
-func _on_resume_button_pressed():
-	resume()
-
-func _on_restart_button_pressed():
+func _on_main_menu_pressed():
 	get_tree().paused = false
-	get_tree().reload_current_scene()
-
-func _on_main_menu_button_pressed():
-	get_tree().paused = false
+	visible = false
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
-
-func _on_settings_button_pressed():
-	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/settings_menu.tscn")

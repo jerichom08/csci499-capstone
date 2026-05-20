ded141810be49a310be23c3c433b357c9f45c0bc
Jericho <jerichomaniago@gmail.com>
2026-05-19 21:54:58 -0400

    update coin system
    - each coin now has a unique id assigned to it
    - fixes issue with collected coins respawning when revisiting a room in level 4

--- scripts/lvls/coin.gd
+++ scripts/lvls/coin.gd
@@ -1,15 +1,25 @@
 extends Area2D
 
+@export var coin_id: String = ""
 @export var value: int = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if coin_id == "":
+		print("Coin has no ID")
+	
+	if CoinManager.is_coin_collected(coin_id):
+		queue_free()
+		return
+	
 	$AnimatedSprite2D.play("spin")
+	
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		CoinManager.add_room_coin(1)
+		CoinManager.add_room_coin(value)
+		CoinManager.mark_coin_collected(coin_id)
 		
 		$AudioStreamPlayer2D.play()
 		$CollisionShape2D.set_deferred("disabled", true)
